b14be11543c1aeb11fb90b9446f4f7ad2c282235
Tomek Mrugalski <tomasz@isc.org>
2019-11-06 00:37:20 +0100

    The distance to the nearest poi is now calculated

--- app/src/main/java/com/example/gnss/ThGpsListener.java
+++ app/src/main/java/com/example/gnss/ThGpsListener.java
@@ -61,9 +61,48 @@ public class ThGpsListener implements LocationListener {
 
         Date d;
         d = new Date(location.getTime());
-        info = "" + d.toString() + "\n" + info;
+        info = "" + d.toString() + "\n" + info + "\n" + "Nearest POI: " + getNearestPoi(location).getText();
         tv.setText(info);
         prevLocation= location;
+    }
+
+    Poi getNearestPoi(Location l) {
+        Poi nearest = null;
+        Double dist = 40000000.0;
+        for (Poi p: PoiList.getInstance().pois_) {
+            if (nearest == null) {
+                nearest = p;
+                continue;
+            }
+            Double tmp = dist(l.getLongitude(), l.getLatitude(), p.lon, p.lat);
+            if (tmp < dist) {
+                dist = tmp;
+                nearest = p;
+                continue;
+            }
+        }
+
+        return nearest;
+    }
+
+    Double degreesToRadians(Double degrees) {
+        return degrees * Math.PI / 180;
+    }
+
+    Double dist(double long1, double lat1, double long2, double lat2) {
+
+        Double earthRadiusKm = 6371.0;
+
+        Double dLat = degreesToRadians(lat2-lat1);
+        Double dLon = degreesToRadians(long2-long1);
+
+        lat1 = degreesToRadians(lat1);
+        lat2 = degreesToRadians(lat2);
+
+        Double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+        Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        return earthRadiusKm * c;
     }
 }
 
