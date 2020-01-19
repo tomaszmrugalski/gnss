f9888f189ad51608cc6ec01202186598b748470b
Tomek Mrugalski <thomson@klub.com.pl>
2020-01-19 20:41:23 +0100

    Commented out some GPS/NMEA stuff

--- app/src/main/java/com/example/gnss/MainActivity.java
+++ app/src/main/java/com/example/gnss/MainActivity.java
@@ -35,30 +35,18 @@ public class MainActivity extends AppCompatActivity {
         String MY_PREFS_NAME = "prefs.txt";
         SharedPreferences.Editor pref = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
 
-        gpsListener = new ThGpsListener();
-        gpsListener.tv = findViewById(R.id.tv);
-        gpsListener.debug = findViewById(R.id.nmea);
-        gpsListener.pref = pref;
-
-
         TextView nmea = findViewById(R.id.nmea);
         nmea.setMovementMethod(new ScrollingMovementMethod());
 
-        nmeaListener = new ThNMEAListener();
-        nmeaListener.debug = nmea;
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},0);
-            return;
-        }
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ) {
-            requestPermissions(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE},0);
-            return;
-        }
-
+        /*
+         nmeaListener = new ThNMEAListener();
+         nmeaListener.debug = nmea;
+                gpsListener = new ThGpsListener();
+                gpsListener.tv = findViewById(R.id.tv);
+                gpsListener.debug = findViewById(R.id.nmea);
+                gpsListener.pref = pref;
         registerListener();
+        */
 
         Button bt1 = (Button) findViewById(R.id.button);
         bt1.setOnClickListener(new View.OnClickListener() {
@@ -71,9 +59,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected void registerListener() {
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, gpsListener);
 
-        lm.addNmeaListener(nmeaListener);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, gpsListener);
+            lm.addNmeaListener(nmeaListener);
+        }
     }
 
 
